cfb348c9daa1c6b7bf887bffe593881c3897db20
KiberCat <maximpodymov@yandex.ru>
2021-05-11 00:37:21 +0300

    Add utils

---
+++ src/enums.ts
@@ -0,0 +1,12 @@
+export enum GameStatuses {
+  Win = 'Win',
+  Losing = 'Losing',
+  Plaing = 'Plaing'
+}
+
+export enum Directions {
+  Top = 'Top',
+  Right = 'Right',
+  Bottom = 'Bottom',
+  Left = 'Left'
+}
---
+++ src/types.ts
@@ -0,0 +1,4 @@
+export type Coordinate = {
+  y: number
+  x: number
+}
